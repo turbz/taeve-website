787604c44bd0c6b7bfe325a31286a8d4b190b7ce
thabisoddx <thabisomoha@gmail.com>
2020-05-12 07:43:45 +0200

    Updates

---
+++ precache-manifest.7f97251b8423646b673c99cffb742d1d.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c40e74058fd9d8df00ae4eda467143e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70f4781c1f393146655a",
+    "url": "/static/css/2.c1fbfcd2.chunk.css"
+  },
+  {
+    "revision": "164a0ae7018f241c91e2",
+    "url": "/static/css/main.8e2f7e79.chunk.css"
+  },
+  {
+    "revision": "70f4781c1f393146655a",
+    "url": "/static/js/2.f2e0c00a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f2e0c00a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "164a0ae7018f241c91e2",
+    "url": "/static/js/main.517386f9.chunk.js"
+  },
+  {
+    "revision": "77c21fdf50ad613a02d2",
+    "url": "/static/js/runtime-main.7686a62c.js"
+  },
+  {
+    "revision": "4716c2367b5ee191b4c09790b78531cd",
+    "url": "/static/media/bg-banner.4716c236.png"
+  },
+  {
+    "revision": "a32e6de82eefe0c9be93734d37c3273a",
+    "url": "/static/media/bg-ipad-nav@2x.a32e6de8.png"
+  },
+  {
+    "revision": "6c48902322049f2506e5d765e1bdd9e8",
+    "url": "/static/media/bg-ipad@2x.6c489023.png"
+  },
+  {
+    "revision": "7445865b7d0b75cfe8530bf1b7937cc2",
+    "url": "/static/media/bg-services.7445865b.svg"
+  },
+  {
+    "revision": "9177f07116a90dce0af46d6434eeff3a",
+    "url": "/static/media/customer-icon.9177f071.svg"
+  },
+  {
+    "revision": "3d129382121e65977c838aae6999aabf",
+    "url": "/static/media/exit-white.3d129382.svg"
+  },
+  {
+    "revision": "3727299debb312d977c39afa4de0048a",
+    "url": "/static/media/exit.3727299d.svg"
+  },
+  {
+    "revision": "97de0aa6eb9d584aac00695c3cfc7a95",
+    "url": "/static/media/facebook.97de0aa6.svg"
+  },
+  {
+    "revision": "2429634bfa97dd14d588d51d9db20e8f",
+    "url": "/static/media/hr-atoms.2429634b.png"
+  },
+  {
+    "revision": "fa17d4881414458498ca6000e587b04c",
+    "url": "/static/media/imageReporting.fa17d488.png"
+  },
+  {
+    "revision": "72baed58481503907485895972f33461",
+    "url": "/static/media/imageTraining.72baed58.png"
+  },
+  {
+    "revision": "33b14d8ea82efafc63441daca12e45a7",
+    "url": "/static/media/job.33b14d8e.svg"
+  },
+  {
+    "revision": "0bf2b9a5d2e0cfe0d21f02dc5a2dabf0",
+    "url": "/static/media/linkedin-icon.0bf2b9a5.svg"
+  },
+  {
+    "revision": "69096082dbfea1eb537e2d28c58a0b57",
+    "url": "/static/media/logo-full-color.69096082.svg"
+  },
+  {
+    "revision": "ec5bad2e6e297056d27ad614cd0523d8",
+    "url": "/static/media/menu-icon.ec5bad2e.svg"
+  },
+  {
+    "revision": "dfda738a9968d6803ef87c8ddfdf7638",
+    "url": "/static/media/office.dfda738a.svg"
+  },
+  {
+    "revision": "667e8ccb78b6c752863ab2deb0536d6b",
+    "url": "/static/media/password.667e8ccb.svg"
+  },
+  {
+    "revision": "1acb7679dfd9be273c673cdf2a6d0656",
+    "url": "/static/media/recruiting-atoms.1acb7679.png"
+  },
+  {
+    "revision": "aa137f8ab2aa53f48d45cc1196bea56b",
+    "url": "/static/media/reporting-icon.aa137f8a.svg"
+  },
+  {
+    "revision": "86b77da616f7cee4aa07e33a1c60a094",
+    "url": "/static/media/smartphone.86b77da6.svg"
+  },
+  {
+    "revision": "3d03ad2f59eacb0de9143afc1f946a19",
+    "url": "/static/media/tick.3d03ad2f.svg"
+  },
+  {
+    "revision": "7303a9587042115530d2bb28f8f1a574",
+    "url": "/static/media/twitter.7303a958.svg"
+  },
+  {
+    "revision": "a9c207171aa669b8e42f497e86bf0d41",
+    "url": "/static/media/user-friendly-icon.a9c20717.svg"
+  },
+  {
+    "revision": "d7c926fa0d87febd40babb4731f7db07",
+    "url": "/static/media/work.d7c926fa.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/taevo-website/precache-manifest.a6e45c212ea505d24aba21ea7ddfd198.js"
+  "/precache-manifest.7f97251b8423646b673c99cffb742d1d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/taevo-website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
